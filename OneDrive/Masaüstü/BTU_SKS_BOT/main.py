2d04fbc251fcb0ec06722eacdadf10e5cc6709f3
Fatih Parmaksız <fatihparm6@gmail.com>
2024-03-26 13:55:24 +0300

    Updated main and getmenu

---
+++ OneDrive/Masaüstü/BTU_SKS_BOT/main.py
@@ -0,0 +1,177 @@
+import getmenu
+import scrape
+from model import Models
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, CallbackContext
+from telegram import Update
+import logging
+import datetime
+import os
+from datetime import time
+import requests
+import dotenv
+dotenv.load_dotenv()
+
+try:
+  menuList = getmenu.Menu().getFormattedMenu()
+except:
+  scrape.ScrapeMenu().getPdf()
+  menuList = getmenu.Menu().getFormattedMenu()
+
+Token = os.getenv("TOKEN")
+
+models = Models()
+models.create_table()
+
+logging.basicConfig(
+  format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+  level=logging.INFO
+)
+
+def restartEveryDay():
+  global menuList
+  os.system("rm pdf0.pdf")
+  time.sleep(1)
+  scrape.ScrapeMenu().getPdf()
+  menuList = getmenu.Menu().getFormattedMenu()
+
+def callbackRestartEveryday(context: CallbackContext):
+  timer = time(hour=6, minute=0)
+  context.job_queue.run_daily(restartEveryDay, timer, days=(0,1,2,3,4,5,6))
+                              
+async def getMenu(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  try:
+    if context.args == []:
+      userInput = datetime.datetime.now().day
+    else:
+      userInput = context.args[0]
+    if (int(userInput) > 0):
+        daysMenu = menuList[int(userInput)-1]
+        await context.bot.send_message(chat_id=update.effective_chat.id, text = daysMenu)
+  except(IndexError, ValueError):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text = "Lütfen geçerli bir gün giriniz.")
+  info = update.message
+  messages_to_add(info)
+
+async def yemekhane(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  user = update.message.from_user
+  print('You talk with user {} and his user ID: {} and his name is {}'.format(
+    user['username'], user['id'], user['first_name']))
+  reply_text = """
+    /menu gün -> girdiğiniz günün menüsünü görebilirsiniz, gün girmezseniz içinde olduğunuz günün menüsünü görebilirsiniz.\n\n/abonelik  -> botumuzda abonelik başlatarak her gün saat 09.00'da botumuzdan menüyü telegram'dan özel mesaj olarak alabilirsiniz.(Sadece /abonelik yazarak günlük mesaj alamazsınız botun kendisine tıklayıp mesajlaşma başlatmanız gerekmektedir)\n\n/abonelikiptal -> aboneliğinizi iptal eder.
+    """
+  await context.bot.send_message(chat_id=update.effective_chat.id, text = reply_text)
+  info = update.message
+  messages_to_add(info)
+
+async def text(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  await context.bot.send_message(chat_id=update.effective_chat.id, text = "Bu bir text mesajıdır")
+  
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  text2 = "Bursa Teknik Üniversitesi Yemekhane Telegram botuna hoşgeldiniz. /help yazarak komutlara erişebilirsiniz.!"
+  await context.bot.send_message(chat_id=update.effective_chat.id, text = text2)
+  info = update.message
+  messages_to_add(info)
+
+async def duyurular(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  admin_id = update.effective_user.id
+  admin_list = os.getenv("ADMIN_LIST")
+
+  if admin_id in admin_list:
+    await context.bot.send_message(chat_id=update.effective_chat.id, text = "Duyuru yapmak için /duyuru <mesaj> komutunu kullanın")
+  else:
+    await context.bot.send_message(chat_id=update.effective_chat.id, text = "Bu komutu kullanmaya yetkiniz yok")
+
+def sendDaysMenu():
+  kayitliKisiListesi = models.check_all()
+  userInput = datetime.datetime.now().day
+  daysMenu = menuList[int(userInput)-1]
+  daysMenuText = daysMenu
+
+  for eachPerson in range(len(kayitliKisiListesi)):
+    telegramId = kayitliKisiListesi[eachPerson][0]
+    try:
+      url = f"https://api.telegram.org/bot{Token}/sendMessage?chat_id={telegramId}&text={daysMenuText}"
+      requests.get(url).json()
+    except:
+      text1 = f"{telegramId} abone olmus ama yetki vermemis"
+      print(text1)
+  
+def callbackMenu(context: CallbackContext):
+  timer = time(hour=9, minute=34)
+  context.job_queue.run_daily(sendDaysMenu, timer, days=(0,1,2,3,4,5,6))
+  
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  user = update.message.from_user
+  print('You talk with user {} and his user ID: {} and his name is {}'.format(
+    user['username'], user['id'], user['first_name']))
+  message = """
+    /menu gün -> girdiğiniz günün menüsünü görebilirsiniz, gün girmezseniz içinde olduğunuz günün menüsünü görebilirsiniz.\n\n/abonelik  -> botumuzda abonelik başlatarak her gün saat 09.00'da botumuzdan menüyü telegram'dan özel mesaj olarak alabilirsiniz.(Sadece /abonelik yazarak günlük mesaj alamazsınız botun kendisine tıklayıp mesajlaşma başlatmanız gerekmektedir)\n\n/abonelikiptal -> aboneliğinizi iptal eder.
+    """
+  await context.bot.send_message(chat_id=update.effective_chat.id, text = message)
+  info = update.message
+  messages_to_add(info)
+
+def abonelik(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  user = update.message.from_user
+  first_name = user["first_name"]
+  last_name = user["last_name"]
+  telegramId = user["id"]
+  print(type(telegramId))
+  check_id = models.check_person(telegramId)
+  try:  
+    if check_id is None:
+      models.add_user(telegramId, first_name, last_name)
+      text = "Abonelik kaydınız oluşturuldu! Her gün Saat 09:00'da günün menüsü sizinle paylaşılacaktır."
+      url = f"https://api.telegram.org/bot{Token}/sendMessage?chat_id={telegramId}&text={text}"
+      requests.get(url).json()
+    else:
+      text = "Zaten aboneliğiniz bulunmaktadır."
+      url = f"https://api.telegram.org/bot{Token}/sendMessage?chat_id={telegramId}&text={text}"
+      requests.get(url).json()
+  except(TypeError):
+    print("hata")
+  info = update.message
+  messages_to_add(info)
+
+def abonelikiptal(update:Update, context:ContextTypes.DEFAULT_TYPE):
+  user = update.message.from_user
+  telegramId= user["id"]
+  print(telegramId)
+  check_id = models.check_person(telegramId)
+  print(check_id)
+  if check_id is None:
+    text = "Aboneliğiniz bulunmamaktadır."
+    url = f"https://api.telegram.org/bot{Token}/sendMessage?chat_id={telegramId}&text={text}"
+    requests.get(url).json()
+  else:
+    models.delete_person(telegramId)
+    text = "Aboneliğiniz iptal edilmiştir."
+    url = f"https://api.telegram.org/bot{Token}/sendMessage?chat_id={telegramId}&text={text}"
+    requests.get(url).json()
+  info = update.message
+  messages_to_add(info)
+
+def messages_to_add(info):
+  user = info.from_user
+  first_name = user["first_name"]
+  last_name = user["last_name"]
+  telegramId = user["id"]
+  message = info.text
+  models.add_message(telegramId, first_name, last_name, message)
+
+def main():
+  app = ApplicationBuilder().token(Token).build()
+  app.add_handler(CommandHandler("start", start))
+  app.add_handler(CommandHandler('help',help))
+  app.add_handler(CommandHandler('menu',getMenu))
+  app.add_handler(CommandHandler('yemekhane',yemekhane))
+  app.add_handler(CommandHandler('abonelik',abonelik))
+  app.add_handler(CommandHandler('abonelikiptal',abonelikiptal))
+  callbackRestartEveryday(app)
+  callbackMenu(app)
+  app.run_polling(allowed_updates=Update.ALL_TYPES)
+
+if __name__ == '__main__':
+  main()
---
+++ OneDrive/Masaüstü/BTU_SKS_BOT/model.py
@@ -0,0 +1,148 @@
+import sqlite3
+from datetime import datetime
+import requests
+from dotenv import load_dotenv
+import os
+import getmenu
+
+load_dotenv()
+Token = os.getenv("TOKEN")
+class Models:
+
+  def __init__(self):
+    self.db = sqlite3.connect('database.db', check_same_thread=False)
+    self.cursor = self.db.cursor()
+    self.create_table()
+
+  def create_table(self):
+    """Creates table if not exists"""
+    self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS subscriptions (
+            chat_id TEXT PRIMARY KEY NOT NULL, 
+            first_name TEXT DEFAULT "",
+            last_name TEXT DEFAULT "",
+            subsdate DATETIME DEFAULT CURRENT_TIMESTAMP
+            )
+        ''')
+    self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS messages (
+            id INTEGER PRIMARY KEY,
+            chat_id TEXT NOT NULL, 
+            first_name TEXT DEFAULT "",
+            last_name TEXT DEFAULT "",
+            message TEXT DEFAULT "",
+            message_date DATETIME DEFAULT CURRENT_TIMESTAMP
+            )
+        ''')
+
+    self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS announcements (
+            id INTEGER PRIMARY KEY,
+            title TEXT DEFAULT "",
+            link TEXT DEFAULT "",
+            publish_date DATETIME DEFAULT CURRENT_TIMESTAMP
+            )
+        ''')
+    self.db.commit()
+
+  def add_user(self, chat_id, first_name, last_name):
+    """Adds people to database"""
+    self.cursor.execute(
+      '''
+        INSERT INTO subscriptions (chat_id, first_name, last_name) VALUES (?, ?, ?)
+        ''', (chat_id, first_name, last_name))
+    self.db.commit()
+
+  def delete_person(self, chat_id):
+    """Deletes people from database"""
+    self.cursor.execute(
+      '''
+        DELETE FROM subscriptions WHERE chat_id = ?
+        ''', (chat_id, ))
+    self.db.commit()
+
+  def check_person(self, chat_id):
+    """Checks if people exists"""
+    self.cursor.execute(
+      '''
+        SELECT * FROM subscriptions WHERE chat_id = ?
+        ''', (chat_id, ))
+    return self.cursor.fetchone()
+
+  def check_all(self):
+    self.cursor.execute('''
+        SELECT * FROM subscriptions 
+        ''')
+    return self.cursor.fetchall()
+
+  def add_message(self, chat_id, first_name, last_name, message):
+    self.cursor.execute(
+      "INSERT INTO messages (chat_id, first_name, last_name,message) VALUES (?, ?, ?, ?)",
+      (chat_id, first_name, last_name, message))
+    self.db.commit()
+
+  def get_all_messages(self):
+    self.cursor.execute('''
+        SELECT * FROM messages
+        ''')
+    return self.cursor.fetchall()
+
+  
+  def add_announcement(self,id,title, link,publish_date):
+    """Adds announcements to database"""
+    self.cursor.execute(
+      '''
+        INSERT INTO announcements (id, title, link,publish_date) VALUES (?, ?, ?, ?)
+        ''', (id, title, link,publish_date))
+    self.db.commit()
+  def delete_announcement(self,id):
+    """Deletes announcements from database"""
+    self.cursor.execute(
+      '''
+        DELETE FROM announcements WHERE id = ?
+        ''', (id,))
+    self.db.commit()
+
+  def check_all_announcements(self):
+    self.cursor.execute('''
+        SELECT id,title FROM announcements
+        ''')
+    return self.cursor.fetchall()
+  
+  def admin_get_all_announcement(self):
+      self.cursor.execute('''
+        SELECT * FROM announcements
+        ''')
+      return self.cursor.fetchall()
+
+  def admin_send_one_announcement(self,id):
+
+    self.cursor.execute('''
+        SELECT * FROM announcements WHERE id = ?
+        ''',(id,))
+    return self.cursor.fetchone()
+
+class Scripts:
+  def __init__():
+    pass
+
+  def push_daily_message():
+    model = Models()
+    menuList, date = getmenu.Menu().getFormattedMenu()
+    userInput = datetime.now().day
+    daysDate = (date[int(userInput)] + " Tarihli Günün Menüsü")
+    daysMenu = menuList[int(userInput)]
+    daysMenuText = daysDate + "\n" + daysMenu
+    all_subscribers = model.check_all()
+    for eachPerson in range(len(all_subscribers)):
+      telegramId = all_subscribers[eachPerson][0]
+      try:
+        url = f"https://api.telegram.org/bot{Token}/sendMessage?chat_id={telegramId}&text={daysMenuText}"
+        req = requests.get(url).json()
+      except:
+        print(f"{telegramId} abone olmus ama yetki vermemis")
+
+
+
+s = Models()
+s.create_table()
